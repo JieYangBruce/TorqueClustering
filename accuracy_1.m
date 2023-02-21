57ec9b5be07392989d913b9348e85b023de0511f
JieYangBruce <115402383+JieYangBruce@users.noreply.github.com>
2023-02-22 01:19:10 +1100

    Update accuracy_1.m

--- accuracy_1.m
+++ accuracy_1.m
@@ -1,6 +1,9 @@
 function score = accuracy_1(true_labels, cluster_labels)
-%ACCURACY Compute clustering accuracy using the true and cluster labels and
-%在原有函数基础上，把hungarian替换掉，加速
+%ACCURACY_1 Compute clustering accuracy using the true and cluster labels and
+%accuracy_1 function is the accelerated version of accuracy function. 
+%Here, we replaced the old Hungarian (munckres) function with
+%the accelerated version of the Hungarian (munckres) function,
+%which is from https://ww2.mathworks.cn/matlabcentral/fileexchange/20328-munkres-assignment-algorithm
 %   return the value in 'score'.
 %
 %   Input  : true_labels    : N-by-1 vector containing true labels
